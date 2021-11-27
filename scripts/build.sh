804d22d8d75a4cbf49f6017afb1fe8974fd2de84
Makoto Onuki <makoto.onuki@gmail.com>
2021-11-27 15:26:19 -0800

    Revert "Revert "Rename the dist directory""
    This reverts commit 023e2b6d3cda54efef60579df7cd432b40da169c.

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,11 +4,9 @@ set -e
 
 cd "${0%/*}/.." # Move to the top dir
 
-deploy_dest=~/cbin/omakoto.github.io/mvv/
-
 echo -n 'Building MVV... '
 
 tsc
-cp -pr README.md LICENSE src/*.html src/*.css src/popbox/ src/res/ dist/
+cp -pr README.md LICENSE src/*.html src/*.css src/popbox/ src/res/ docs/
 
 echo "Success"
