eba30ea946c4a4067f4b081006386aa52f1e0c1c
Makoto Onuki <makoto.onuki@gmail.com>
2021-11-27 13:50:07 -0800

    Support auto deploy

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,34 @@ set -e
 
 cd "${0%/*}/.." # Move to the top dir
 
-echo 'Building MVV...'
+deploy_dest=~/cbin/omakoto.github.io/mvv/
 
-tsc
-cp -pr README.md LICENSE src/*.html src/*.css src/popbox/ src/res/ dist/
+build() {
+    echo -n 'Building MVV... '
+
+    tsc
+    cp -pr README.md LICENSE src/*.html src/*.css src/popbox/ src/res/ dist/
+
+    echo "Success"
+}
+
+deploy() {
+    echo -n 'Deploying MVV... '
+
+    if ! [[ -d "$deploy_dest" ]] ; then
+        echo "Target directory \"$deploy_dest\" not found" 1>&2
+        return 1
+    fi
+
+    rm -fr "$deploy_dest/*"
+
+    mkdir -p "$deploy_dest"
+    cp -pr dist/* "$deploy_dest"
+
+    echo "Success"
+}
+
+build
+if [[ "$1" == -d ]] || (( $MVV_ALWAYS_DEPLOY + 0 )) ; then
+    deploy
+fi
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-set -e
-
-dest=~/cbin/omakoto.github.io/mvv/
-
-cd "${0%/*}/.." # Move to the top dir
-
-./scripts/build.sh
-
-echo 'Deploying MVV...'
-
-rm -fr "$dest/*"
-
-mkdir -p "$dest"
-cp -pr dist/* "$dest"
+"${0%/*}/"build.sh -d
