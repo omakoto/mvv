f5faf387b49833efd7d8926050e330a9de29b538
Makoto Onuki <makoto.onuki@gmail.com>
2021-11-27 15:26:48 -0800

    README.md no longer needed

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,6 @@ cd "${0%/*}/.." # Move to the top dir
 echo -n 'Building MVV... '
 
 tsc
-cp -pr README.md LICENSE src/*.html src/*.css src/popbox/ src/res/ docs/
+cp -pr LICENSE src/*.html src/*.css src/popbox/ src/res/ docs/
 
 echo "Success"
