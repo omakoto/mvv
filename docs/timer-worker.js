f7d4a7c6133984b5f31d79fe1d05a560df848eb9
Makoto Onuki <makoto.onuki@google.com>
2021-12-04 09:42:28 -0800

    Handle PC suspend nicely

--- docs/timer-worker.js
+++ docs/timer-worker.js
@@ -7,9 +7,15 @@ onmessage = (e) => {
         console.log("Setting timer with interval: " + data.interval + " ms, result:", data.result);
         let next = performance.now();
         const tick = () => {
+            const now = performance.now();
             next += data.interval;
+            if (next <= now) {
+                // CPU couldn't keep up, or the device suspended.
+                console.log("Adjusting the timer, did PC get suspended?");
+                next = now + data.interval;
+            }
             postMessage(data.result);
-            setTimeout(tick, next - performance.now());
+            setTimeout(tick, next - now);
         };
         tick();
     }
