a4af07485b935f255455d365754e3409d1f46179
Makoto Onuki <makoto.onuki@gmail.com>
2025-07-01 20:27:02 -0700

    Enable typescript source map

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,7 +10,9 @@ rm -f src/res/*~
 rm -fr docs/
 mkdir -p docs/
 mkdir -p docs/res/
+mkdir -p docs/src/
 npx tsc
 cp -pr LICENSE src/*.html src/*.css src/popbox/ src/res js/* docs/
+cp -pr src/*.ts docs/src/
 
 echo "Success"
