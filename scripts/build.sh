b3c97aa778d7a444f304ade95221897dbd68a521
Makoto Onuki <makoto.onuki@google.com>
2025-07-03 14:40:57 -0700

    Revert "Change top directory name"
    This reverts commit 76d82233d0c1ffc4fab870f0d1ea6c04e41358c6.
    
    Actually, no, the directory name is fixed for github pages.

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,14 +6,13 @@ cd "${0%/*}/.." # Move to the top dir
 
 echo -n 'Building MVV... '
 
-outdir=serve-root/
-
-rm -fr ${outdir}
-mkdir -p ${outdir}
-mkdir -p ${outdir}res/
-mkdir -p ${outdir}src/
+rm -f src/res/*~
+rm -fr docs/
+mkdir -p docs/
+mkdir -p docs/res/
+mkdir -p docs/src/
 npx tsc
-cp -pr LICENSE src/*.html src/*.css src/popbox/ src/res js/* ${outdir}
-cp -pr src/*.ts ${outdir}src/
+cp -pr LICENSE src/*.html src/*.css src/popbox/ src/res js/* docs/
+cp -pr src/*.ts docs/src/
 
 echo "Success"
--- scripts/start.sh
+++ scripts/start.sh
@@ -6,7 +6,7 @@ cd "${0%/*}/.." # Move to the top dir
 
 ./scripts/build.sh
 
-cd serve-root
+cd docs
 
 port=11080
 (
